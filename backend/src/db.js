be87b496a508dcc628c5bc9f3cd683589a941ea1
Juan Jose <chambijuanjose05@gmail.com>
2023-11-17 21:15:10 -0300

    Fix/Base de Datos Conectada

--- backend/src/db.js
+++ backend/src/db.js
@@ -3,12 +3,12 @@ require("dotenv").config();
 const { MONGODB_URI } = process.env;
 
 
-async function handlerDataBase () {
-    try {
-        mongoose.connect(MONGODB_URI);
-        console.log("Conected");
-    } catch (error) {
-        console.log("error al conectar bases de datos:", error);
-    }
-}
-handlerDataBase();
+mongoose.connect(MONGODB_URI).then(() => console.log("Conectado"))
+
+// o
+
+// mongoose.connect("mongodb://localhost:27017/schooldb",{
+// family: 4,
+// }).then(() => {
+//     console.log("connection successfully ");
+// })
