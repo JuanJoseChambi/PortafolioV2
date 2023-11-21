01a5994c75d79253ad2704b51bb8441c6a2a9739
Juan Jose <chambijuanjose05@gmail.com>
2023-11-20 21:19:05 -0300

    Feat/Base Routes Proyects

--- backend/src/controllers/proyectosCtrl.js
+++ backend/src/controllers/proyectosCtrl.js
@@ -7,45 +7,10 @@ proyectosRouter.get("/", async (req, res) => {
     const allDB = await Proyecto.find({})
     res.status(200).json(allDB)
 })  
-proyectosRouter
-    .route("/:id") //Route permite hacer para un solo path varios metodos, como en este caso get, patch y delete que nesecitan id
-    .get(async (req, res) => {
-        const { id } = req.params
-
-        const proyect = await Proyecto.findById(id);
-
-        res.status(200).json(proyect)
-    })
-    .patch(async (req, res) => { //Patch permite cambiar propiedades especificas y no hacer un cambio completo de todo
-        const { id } = req.params
-        const update = req.body
-
-        await Proyecto.findByIdAndUpdate(id, update)
-
-        res.status(200).json({status: "Success"})
-    })
-    .put(async (req, res) => {
-        const {id} = req.params
-        const update = req.body
-
-        // const result = await Proyecto.findByIdAndUpdate(id, update, {new: true}) // new:true le indica a Mongoose que devuelva el documento actualizado en lugar del documento original., viene por defecto en false
-       
-        const result = await Proyecto.findById(id)
-        update.nombre?result.nombre = update.nombre : result.nombre;
-        update.edad?result.edad = update.edad : result.edad;
-        await result.save()
-        
-        res.status(200).json(result)
-    })
-    .delete(async (req, res) => {
-        const {id} = req.params
-        await Proyecto.findByIdAndDelete(id)
-        res.status(200).json({status:"success"})
-    })
 
 proyectosRouter.post("/", async (req, res) => {
     const proyectoTech  = req.body;
-    // console.log(proyectoTech);
+
     const result = await Proyecto.create(proyectoTech)
 
     res.status(200).json({ result: "Success", result})
@@ -59,3 +24,45 @@ proyectosRouter.put("/", async (req, res) => {
 })
 
 module.exports = proyectosRouter
+
+
+
+
+
+
+// USO DE ROUTER Y PATCH
+// proyectosRouter
+//     .route("/:id") //Route permite hacer para un solo path varios metodos, como en este caso get, patch y delete que nesecitan id
+//     .get(async (req, res) => {
+//         const { id } = req.params
+
+//         const proyect = await Proyecto.findById(id);
+
+//         res.status(200).json(proyect)
+//     })
+//     .patch(async (req, res) => { //Patch permite cambiar propiedades especificas y no hacer un cambio completo de todo
+//         const { id } = req.params
+//         const update = req.body
+
+//         await Proyecto.findByIdAndUpdate(id, update)
+
+//         res.status(200).json({status: "Success"})
+//     })
+//     .put(async (req, res) => {
+//         const {id} = req.params
+//         const update = req.body
+
+//         const proyectoAct = await Proyecto.findByIdAndUpdate(id, update, {new: true}) // new:true le indica a Mongoose que devuelva el documento actualizado en lugar del documento original., viene por defecto en false
+       
+//         // const result = await Proyecto.findById(id)
+//         // update.nombre?result.nombre = update.nombre : result.nombre;
+//         // update.edad?result.edad = update.edad : result.edad;
+//         // await result.save()
+        
+//         res.status(200).json(proyectoAct)
+//     })
+//     .delete(async (req, res) => {
+//         const {id} = req.params
+//         await Proyecto.findByIdAndDelete(id)
+//         res.status(200).json({status:"success"})
+//     })
--- backend/src/models/Proyectos.js
+++ backend/src/models/Proyectos.js
@@ -1,43 +1,38 @@
 const mongoose = require("mongoose")
 
 const proyectoSchema = new mongoose.Schema({
-    // imagenes:{
-    //     type: String
-    // },
-    // titulo: {
-    //     type: String,
-    //     required: true,
-    //     // unique:true //que sea unico 
-    // },
-    // descripcion: {
-    //     type: String,
-    //     required:true
-    // },
-    // duracion: {
-    //     type: String,
-    //     required: true
-    // },
-    // front: {
-    //     type: String
-    // },
-    // backend: {
-    //     type: String
-    // },
-    // db: {
-    //     type: String
-    // },
-    // github: {
-    //     type: String
-    // },
-    // web: {
-    //     type: String
-    // }
-    nombre:{
-        type:String
+    imagenes:{
+        type: String
     },
-    edad:{
-        type:Number
+    titulo: {
+        type: String,
+        required: true,
+        // unique:true //que sea unico 
+    },
+    descripcion: {
+        type: String,
+        required:true
+    },
+    duracion: {
+        type: String,
+        required: true
+    },
+    front: {
+        type: String
+    },
+    backend: {
+        type: String
+    },
+    db: {
+        type: String
+    },
+    github: {
+        type: String
+    },
+    web: {
+        type: String
     }
+
 }, {timestamps:true, versionKey: false})
 
 const Proyecto = mongoose.model("proyecto", proyectoSchema)
