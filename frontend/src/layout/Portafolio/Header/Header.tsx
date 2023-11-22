baeb371eb3b33f63d73fbbe2f1dff272d1538336
Juan Jose <chambijuanjose05@gmail.com>
2023-11-21 21:51:56 -0300

    Feat/Image Logo

--- frontend/src/layout/Portafolio/Header/Header.tsx
+++ frontend/src/layout/Portafolio/Header/Header.tsx
@@ -1,3 +1,6 @@
+// import logo from "../../../assets/assetsHeader/Logo 5 Transparent.png"
+import logo from "../../../assets/assetsHeader/logoBien.png"
+
 function Header() {
   return (
     <header className="area relative ">
@@ -10,6 +13,9 @@ function Header() {
       </ul> */}
 
       <section className="flex justify-center items-center flex-col">
+        <picture className="w-[150px]">
+            <img src={logo} alt="" className="w-full" />
+        </picture>
         <h1 className="text-[#DCDEDD] text-7xl font-black tracking-wide drop-shadow-[15px_15px_6px_black]">JUAN JOSE CH</h1>
         <h2 className="text-[#DCDEDD] text-sm font-extralight mt-4">Desarrollador Frontend Junior</h2>
       </section>
