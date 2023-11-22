647aab2f038af777dce32f6bddd6e084f04795dd
Juan Jose <chambijuanjose05@gmail.com>
2023-11-21 21:07:27 -0300

    Feat/Text Shadow

--- frontend/src/layout/Portafolio/Header/Header.tsx
+++ frontend/src/layout/Portafolio/Header/Header.tsx
@@ -10,8 +10,8 @@ function Header() {
       </ul> */}
 
       <section className="flex justify-center items-center flex-col">
-        <h1 className="text-[#DCDEDD] text-7xl font-black tracking-wide ">JUAN JOSE CH</h1>
-        <h2 className="text-[#DCDEDD] text-sm font-extralight">Desarrollador Frontend Junior</h2>
+        <h1 className="text-[#DCDEDD] text-7xl font-black tracking-wide drop-shadow-[15px_15px_6px_black]">JUAN JOSE CH</h1>
+        <h2 className="text-[#DCDEDD] text-sm font-extralight mt-4">Desarrollador Frontend Junior</h2>
       </section>
 
       {/* <ul className="absolute right-0 bottom-0">
