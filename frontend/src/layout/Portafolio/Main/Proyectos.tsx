8335f7a52301d8dbe27300ff5a031b54a316dbf3
Juan Jose <chambijuanjose05@gmail.com>
2023-11-24 23:32:54 -0300

    Bug/Carusel Position

--- frontend/src/layout/Portafolio/Main/Proyectos.tsx
+++ frontend/src/layout/Portafolio/Main/Proyectos.tsx
@@ -7,7 +7,11 @@ function Proyectos() {
     const [cards, setCards] = useState([
         {id:1, image:MMarket},
         {id:2, image:MMarket},
-        {id:3, image:TN}
+        {id:3, image:TN},
+        {id:4, image:MMarket},
+        {id:5, image:MMarket},
+        {id:6, image:MMarket},
+        {id:6, image:MMarket},
     ])
     const [num, setNum] = useState(1)
 
@@ -29,20 +33,20 @@ function Proyectos() {
     }
     
   return (
-    <section className="area min-h-screen snap-center flex flex-col justify-center items-center text-white py-20 lg:py-0">
-        <section className=" w-full h-full lg:w-[1080px] lg:min-h-[450px] relative flex flex-wrap justify-evenly items-center space-y-9 lg:space-y-0 flex-col lg:flex-row">
+    <section className="area min-h-screen snap-center relative flex flex-col justify-center items-center text-white py-20 lg:py-0">
+        <section className="bg-red-500 w-[1050px] h-full overflow-hidden lg:w-[1080px] lg:min-h-[450px] flex justify-center items-center space-y-9 lg:space-y-0 lg:space-x-9 flex-col lg:flex-row">
 
-            <i onClick={prevCard} className='bg-red w-[25px] h-[25px] flex justify-center items-center absolute right-0 lg:left-[-20px] cursor-pointer z-10 text-3xl bx bx-chevron-left'></i>
+            <i onClick={prevCard} className='bg-red w-[25px] h-[25px] flex justify-center items-center absolute right-0 lg:left-0 cursor-pointer z-10 text-3xl bx bx-chevron-left'></i>
 
             {cards.map((card, i) => (
-                <picture onClick={() => setNum(i)} key={card.id} className={`cards-opacity ${num === i? "h-[400px] w-[400px] grayscale-0 blur-none cursor-auto": null}`}>
+                <picture onClick={() => setNum(i)} key={card.id} className={`cards-opacity ${num === i? "min-h-[400px] min-w-[400px] grayscale-0 blur-none cursor-auto": null}`}>
                     <div className="absolute top-0 left-0 w-full h-full bg-gradient-to-b from-transparent to-[#1F2025]"></div>
                     <img src={card.image} alt="" />
                 </picture>
             ))
             }
 
-            <i onClick={nextCard} className='bg-red w-[25px] h-[25px] flex justify-center items-center absolute right-0 lg:right-[-20px] cursor-pointer z-10 text-3xl bx bx-chevron-right' ></i>
+            <i onClick={nextCard} className='bg-red w-[25px] h-[25px] flex justify-center items-center absolute right-0 lg:right-0 cursor-pointer z-10 text-3xl bx bx-chevron-right' ></i>
 
         </section>  
         <section className="space-x-10 z-10">
