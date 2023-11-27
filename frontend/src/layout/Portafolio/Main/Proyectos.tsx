5c2a7f9cc096a567fccd3867ba254e37c44c3bec
Juan Jose <chambijuanjose05@gmail.com>
2023-11-26 23:15:26 -0300

    Feat/Info Crads

--- frontend/src/layout/Portafolio/Main/Proyectos.tsx
+++ frontend/src/layout/Portafolio/Main/Proyectos.tsx
@@ -22,8 +22,8 @@ function Proyectos() {
     { id: 0, imagenes: perfil, titulo:"Preoyecto 0", descripcion:"Proyecto Muy Largoooooooo", front:"Javascript", duracion:"25 Jul - 25 Ago", github: "github.com/JuanJose" },
     { id: 1, imagenes: MMarket , titulo:"Preoyecto 1", descripcion:"Proyecto Muy Largoooooooo", front:"Javascript", duracion:"25 Jul - 25 Ago", github: "github.com/JuanJose"   },
     { id: 2, imagenes: TN , titulo:"Preoyecto 2", descripcion:"Proyecto Muy Largoooooooo", front:"Javascript", duracion:"25 Jul - 25 Ago", github: "github.com/JuanJose"  } ,
-    { id: 3, imagenes: perfil , titulo:"Preoyecto 3", descripcion:"MMarket simplifica compras en tiendas locales a través de su plataforma en línea, beneficiando a tiendas y usuarios con opciones de compra en persona y entregas a domicilio. Su objetivo es ampliar su alcance", front:"Javascript", duracion:"25 Jul - 25 Ago", github: "github.com/JuanJose"   },
-    { id: 4, imagenes: TN , titulo:"Preoyecto 4", descripcion:"Desarrollo de página web de recetas con más de 100 recetas detalladas. Los usuarios pueden agregar sus propias recetas, colaborar en una base de datos compartida y pudiendo marcar recetas como favoritas o eliminarlas.", front:"Javascript", duracion:"25 Jul - 25 Ago", github: "github.com/JuanJose"  },
+    { id: 3, imagenes: perfil , titulo:"MMarket", descripcion:"MMarket simplifica compras en tiendas locales a través de su plataforma en línea, beneficiando a tiendas y usuarios con opciones de compra en persona y entregas a domicilio. Su objetivo es ampliar su alcance", front:"JAVASCRIPT REACT TAILWIND REDUX BOOSTRAP ", back:"NODE EXPRESS SEQUELIZE", db:"POSTGRESQL", duracion:"25 Ene - 25 Ago", github: "github.com/JuanJose"   },
+    { id: 4, imagenes: TN , titulo:"RECIPE FOOD", descripcion:"Desarrollo de página web de recetas con más de 100 recetas detalladas. Los usuarios pueden agregar sus propias recetas, colaborar en una base de datos compartida y pudiendo marcar recetas como favoritas o eliminarlas.", front:"TYPESCRIPT-REACT-TAILWIND",back:"NODE-EXPRESS", duracion:"25 Abr - 25 Jul", github: "github.com/JuanJose"  },
     { id: 5, imagenes: MMarket , titulo:"Preoyecto 5", descripcion:"Proyecto Muy Largoooooooo", front:"Javascript", duracion:"25 Jul - 25 Ago", github: "github.com/JuanJose"   },
     { id: 6, imagenes: TN , titulo:"Preoyecto 6", descripcion:"Proyecto Muy Largoooooooo", front:"Javascript", duracion:"25 Jul - 25 Ago", github: "github.com/JuanJose"},
     { id: 7, imagenes: perfil , titulo:"Preoyecto 7", descripcion:"Proyecto Muy Largoooooooo", front:"Javascript", duracion:"25 Jul - 25 Ago", github: "github.com/JuanJose"   },
@@ -31,7 +31,7 @@ function Proyectos() {
 
   const renderCard = (card: Cards, i: number) => (
     <picture onClick={() => setNum(i)} key={card.id} className={`cards-opacity ${num === i ? "min-h-[390px] min-w-[400px] grayscale-0 blur-none cursor-auto pointer-events-none" : ""}`}>
-      <div className="absolute top-0 left-0 w-full h-full bg-gradient-to-b from-transparent to-[#1F2025]"></div>
+      <div className="absolute top-0 left-0 w-full h-full bg-gradient-to-b from-transparent to-[#232429]"></div>
       <img src={card.imagenes} alt="Juan Jose Ch" className="select-none" />
     </picture>
   );
@@ -57,16 +57,18 @@ function Proyectos() {
         i === num || i === num - 1 || i === num + 1 ? renderCard(card, i) : null 
         )}
       </section>
-      <section className="w-[65%] h-[150px] pt-[40px] relative flex justify-center items-center flex-col bg-lime-500">
+      <section className="w-[65%] h-[180px] pt-[25px] relative flex justify-center items-center flex-col bg-limee-500">
         {cards.map((card, i) => 
             i == num 
             ?
             <>
-              <h2 className="absolute top-[-30px] text-6xl font-bold tracking-wider text-white-light">{card.titulo}</h2>
+              <h2 className="absolute top-[-35px] drop-shadow-[1px_1px_40px_black] text-5xl font-bold tracking-wider text-white-light">{card.titulo}</h2>
+              <small className="absolute top-5 font-extralight text-xs">{card.duracion}</small>
               <small className="text-center font-extralight">{card.descripcion}</small>
-              <p>{card.duracion}</p>
-              <p>{card.front}</p>
-              <button>Ver Codigo</button>
+              <p className="absolute left-0 text-xs">{card.front}</p>
+              <p className="absolute right-0 text-xs">{card.back}</p>
+              <p className="absolute bottom-0 text-xs">{card.db}</p>
+              <button className="w-[230px] h-[45px] shadow-xl shadow-black border border-solid border-neutral-900 font-semibold text-white-light tracking-widest text-sm">VER CODIGO</button>
             </> 
             : null
         )}
